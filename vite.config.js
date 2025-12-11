9f07cc9c1d5e8e6d51f7797e49fd2cb18fa274af
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2025-12-11 10:40:32 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // CORRECCIÓN: Agregar barras inclinadas al inicio y al final
-  base: "/portfolio/",
+  base: "portfolio",
 });
