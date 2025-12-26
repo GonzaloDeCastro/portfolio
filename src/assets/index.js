4c8e002cf1f9765cd106c0c7e118af0dbcc15c6b
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2025-12-26 15:15:13 -0300

    clean code

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -14,10 +14,7 @@ import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
 import reactjs from "./tech/reactjs.png";
-import c from "./tech/c.png";
 import python from "./tech/python.png";
-import pandas from "./tech/pandas.svg";
-import excel from "./tech/excel.png";
 import mysql from "./tech/mysql.png";
 import nodejs from "./iconNode.png";
 import redux from "./tech/reduxIcon.svg";
@@ -25,7 +22,6 @@ import mongodb from "./tech/mongodbIcon.svg";
 
 import reactDeveloper from "./company/react.png";
 import webDeveloper from "./company/webDeveloper.png";
-import cDeveloper from "./company/cDeveloper.png";
 import datascience from "./company/datascience.png";
 import pythonIcon2 from "./pythonIcon2.png";
 import windfarmIcon from "./windFarmIcon.png";
@@ -34,13 +30,11 @@ import nuevoLogo from "../../portfolio.png";
 import eventNow from "./eventNow.png";
 import damasOnline from "./damasGame.png";
 import apiMarvel from "./apiMarvel.png";
-import salesanalisis from "./portfolio.jpg";
 import presuflow from "./presuflow.png";
 
 
 import notebook from "./backgroundimages/notebook.jpg";
 export {
-  logo,
   backend,
   creator,
   mobile,
@@ -56,19 +50,14 @@ export {
   javascript,
   reactjs,
   python,
-  excel,
   mysql,
   nodejs,
-  pandas,
   eventNow,
   damasOnline,
   apiMarvel,
-  salesanalisis,
   reactDeveloper,
   webDeveloper,
-  cDeveloper,
   datascience,
-  c,
   windfarmIcon,
   codeIcon,
 
